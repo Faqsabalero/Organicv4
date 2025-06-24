667bea9e0d14fcf6a775aea1f9581f478932ab35
User <user@example.com>
2025-06-24 17:25:05 +0000

    feat: Mejora el diseño de las secciones de productos con imágenes más grandes y mejor presentación visual

--- organic_shops_project/settings.py
+++ organic_shops_project/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = "django-insecure-w+=p)(v)&z65&u#3%7jg9t%(i765+@rrdrsc$o--652ns(sth4
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['0.0.0.0', 'localhost', '127.0.0.1']
 
 
 # Application definition
